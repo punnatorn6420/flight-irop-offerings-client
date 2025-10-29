d4007f54caaee112ab0576757be1facdd37e0d83
Punnatorn Yimpong <Punnatorn.Yim@nokair.co.th>
2025-10-29 14:40:08 +0700

    modify footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -2,7 +2,7 @@
 
 export default function Footer() {
   return (
-    <footer className="py-8 text-center text-[18px] text-grey-800">
+    <footer className="pt-12 text-center text-[18px] text-grey-800">
       © 2017 บริษัท สายการบินนกแอร์ จำกัด (มหาชน)
     </footer>
   );
